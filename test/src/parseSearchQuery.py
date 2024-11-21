30f304a9f7afef7904339e800cdefb9e77d378c6
rspacerr <definitelynotoberon@gmail.com>
2024-11-20 21:52:56 -0500

    aunit tests for parseSearchQuery

--- test/src/parseSearchQuery.py
+++ test/src/parseSearchQuery.py
@@ -12,6 +12,6 @@ nlp = spacy.load("en_core_web_sm")
 
 def parseSearchQuery(query):
     doc = nlp(query)
-    lemmatized = [token.lemma_ for token in doc]
-    #print(lemmatized) Uncomment this line to view the text after being lemmatized.
+    lemmatized = [token.lemma_ for token in doc if not token.is_stop or not token.pos_ == "ADP"]
+    #print(lemmatized) # Uncomment this line to view the text after being lemmatized.
     return lemmatized
--- test/test_parseSearchQuery.py
+++ test/test_parseSearchQuery.py
@@ -9,7 +9,7 @@ class TestSearchQuery(unittest.TestCase):
     This test ensures that a sizeable query using punctuation and other symbols is properly tokenized by checking the length
     of each token. Improper tokenization may lead to punctuation or symbols being incorrectly classified.
 
-    The first test text was provided on page 92 of `Search Engines: Information Retrieval in Practice`.
+    The first test text was provided on page 95 of `Search Engines: Information Retrieval in Practice`.
     The second and third texts were provided by https://spacy.io/usage/spacy-101.
     """
     def test_query_length(self):
@@ -24,26 +24,66 @@ class TestSearchQuery(unittest.TestCase):
     This test ensures that similar words are correctly lemmatized without losing their original meaning, including words with multiple
     stems (fishes, fishing, fisherman, etc).
 
-    The first test text was provided on page 92 of `Search Engines: Information Retrieval in Practice`.
+    The first test text was provided on page 95 of `Search Engines: Information Retrieval in Practice`.
     """
-    def test_multiple_stems(self):
+    def test_lemmatize(self):
         tokenized = parseSearchQuery("Document will describe marketing strategies carried out by U.S. companies for their agricultural "
                                     "chemicals, report predictions for market share of such chemicals, or report market statistics for "
                                     "agrochemicals, pesticide, herbicide, fungicide, insecticide, fertilizer, predicted sales, market share, "
                                     "stimulate demand, price cut, volume of sales")
         self.assertTrue(tokenized[4] == "strategy")
         self.assertTrue(tokenized[-1] == "sale")
-        self.assertTrue(tokenized[-8] == "demand")
-        tokenized = parseSearchQuery("I'm going to go fishing this Saturday with the boys.")
-        self.assertTrue(tokenized[0] == "I")
-        self.assertTrue(tokenized[2] == "go")
-        self.assertTrue(tokenized[4] == "go")
-        tokenized = parseSearchQuery("fish fishes fishing fisherman fish pond phishing")
+        self.assertTrue(tokenized[-7] == "demand")
+        tokenized = parseSearchQuery("fish fishes fishing fisherman fish pond phishing") # stemming test
         self.assertTrue(tokenized[0] == tokenized[1])
         self.assertTrue(tokenized[0] != tokenized[2])
         self.assertTrue(tokenized[0] != tokenized[3])
         
-        ### TODO: further commenting on tests, we should have a test for stop words (may need to look at spaCy docs), a test with empty query, and a test with a single query, at least
+    """
+    Test queries that consist solely of stop words.
+
+    The first test case consists of the first few default stop words as defined by the spaCy library, with punctuation removed.
+    The second test case consists of only stop words but is a sentence with semantic meaning.
+    The third test case consists of a mix of stop words and non-stop words.
+    The fourth test case tests that stop words are removed if they add no semantic context.
+    """
+    def test_stop_words(self):
+        sentence = ("call or even same anyway eight except being thereafter yourself done used "
+                   "very they empty will fifty hence anyhow next please are would off whereby "
+                   "a fifteen anywhere itself five how been whither upon however almost then us "
+                   "she where well be therefore now but ours became meanwhile go we beyond nevertheless")
+        tokenized = parseSearchQuery(sentence)
+        self.assertTrue(len(tokenized) != 0)
+        self.assertTrue(len(tokenized))
+        self.assertTrue(len(parseSearchQuery("to be or not to be")) != 0)
+        sentence = "Hey! This is a random sentence that probably has some stop words inside it, but I'm not sure. Hopefully you can check?"
+        tokenized = parseSearchQuery(sentence)
+        self.assertTrue(len(tokenized) != 0)
+        self.assertTrue(len(tokenized) != len(sentence.split(" ")))
+        self.assertTrue(len(parseSearchQuery("Bank of Australia")) == 2)
+    
+    """
+    Test an empty query. The tokenization process should return an empty string.
+    """
+    def test_empty_query(self):
+        tokenized = parseSearchQuery("")
+        self.assertTrue(len(tokenized) == 0)
+
+    """
+    Test a query with only one input. 
+
+    The first three test cases are with random words.
+    The next two test cases are with stop words.
+    The next two test cases are with conjunctions.
+    """
+    def test_singular_query(self):
+        self.assertTrue(len(parseSearchQuery("dinosaur")) == 1)
+        self.assertTrue(len(parseSearchQuery("Professor")) == 1)
+        self.assertTrue(len(parseSearchQuery("Europe")) == 1)
+        self.assertTrue(len(parseSearchQuery("the")) == 1)
+        self.assertTrue(len(parseSearchQuery("a")) == 1)
+        self.assertTrue(len(parseSearchQuery("I'll")) == 2)
+        self.assertTrue(len(parseSearchQuery("doesn't")) == 2)
 
 if __name__ == "__main__":
     unittest.main()
